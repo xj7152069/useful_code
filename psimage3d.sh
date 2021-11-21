94cd116b3dd03fa18716ad1396b948e9c67058f8
项健 <41769115+xj7152069@users.noreply.github.com>
2021-11-22 00:06:32 +0800

    Update psimage3d.sh

--- psimage3d.sh
+++ psimage3d.sh
@@ -1,7 +1,19 @@
-filename="data3d.tp.bin"
+filename="data3d.rebuild.1.bin"
 vplname="${filename}.vpl"
 epsname="${filename}.eps"
 
+echo n1=1024 d1=0.001 o1=0\
+ n2=200 o2=0 d2=10\
+ n3=200 o3=0 d3=10\
+ in=$filename data_format=native_float>rsftp
+
+sfbyte < rsftp gainpanel=all pclip=99.9 |\
+ sfgrey3 frame1=0 frame2=200 frame3=0 color= flat=n\
+ label1="T(ms)" label2="X(m)" label3="Y(m)"\
+ point1=0.65 point2=0.7 screenratio=1 \
+ >$vplname 
+
+:<<!
 echo n1=1024 d1=0.001 o1=0\
  n2=80 o2=-0.000200 d2=0.000005\
  n3=50 o3=-0.000125 d3=0.000005\
@@ -12,5 +24,6 @@ sfbyte < rsftp gainpanel=all pclip=99.9 |\
  label1="T(ms)" label2="px(s/m)" label3="py(s/m)"\
  point1=0.65 point2=0.7 screenratio=1 \
  >$vplname 
+!
 
 vpconvert $vplname bgcolor=w color=y $epsname
