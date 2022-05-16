9577fd23a9d99c568b008ef0d4d7f826395eaf78
项健 <41769115+xj7152069@users.noreply.github.com>
2022-05-16 19:12:02 +0800

    Update slantstack3d.hpp

--- xj.include/xj.include/slantstack3d.hpp
+++ xj.include/xj.include/slantstack3d.hpp
@@ -46,12 +46,13 @@ void slantstack3d_parset(int ntrace, int nline, int nt,\
 void slantstack3d_parupdate(struct slantstack3d & par)
 { 
     par.datatx.zeros(par.ntrace,par.nline,par.nt);
-    par.datatp.zeros(par.np_trace,par.np_line,par.nt);
     par.recoverdatatx.zeros(par.ntrace,par.nline,par.nt);
-    par.ptrace_coord.zeros(par.np_trace,1);
-    par.pline_coord.zeros(par.np_line,1);
     par.ntrace_coord.zeros(par.ntrace,1);
     par.nline_coord.zeros(par.nline,1);
+    par.datatp.zeros(par.np_trace,par.np_line,par.nt);
+    par.ptrace_coord.zeros(par.np_trace,1);
+    par.pline_coord.zeros(par.np_line,1);
+    
     int k;
     for(k=0;k<par.np_trace;k++)
     {
@@ -79,7 +80,7 @@ void slantstack3d_cleardata(struct slantstack3d & par)
 }
 
 /////////////////////////slantstack_CG3d///////////////////////////
-void slantstack3d_forward_L_operator(fcube &datatp,fcube &datatx,\
+void slantstack3d_stack_L_operator(fcube &datatp,fcube &datatx,\
  fmat &ptrace_coord,fmat &pline_coord,fmat &ntrace_coord,fmat &nline_coord,\
  float dt){
 
@@ -113,11 +114,52 @@ for(k=0;k<n3;k++){
                     }
     }}}}}
 }
-void slantstack3d_forward(struct slantstack3d &par){
-    slantstack3d_forward_L_operator(par.datatp,par.datatx,\
+
+void slantstack3d_recover_LT_operator(fcube &recoverdatatx,fcube &datatp,\
+ fmat &ptrace_coord,fmat &pline_coord,fmat &ntrace_coord,fmat &nline_coord,\
+ float dt){
+
+    int ip,jp,in,jn,k,ntaoceil,ntaofloor;
+    float tao0,dtao1,dtao2,dtao12,ntao12,wceil,wfloor,wsum,wmin(0.000000001);
+    int n1(recoverdatatx.n_rows),n2(recoverdatatx.n_cols),n3(recoverdatatx.n_slices),\
+        np1(datatp.n_rows),np2(datatp.n_cols);
+    recoverdatatx.fill(0.0);
+
+for(k=0;k<n3;k++){
+    tao0=k*dt;
+    for(in=0;in<n1;in++){
+        for(jn=0;jn<n2;jn++){
+            for(ip=0;ip<np1;ip++){
+                dtao1=ntrace_coord(in,0)*ptrace_coord(ip,0);
+                for(jp=0;jp<np2;jp++){
+                    dtao2=nline_coord(jn,0)*pline_coord(jp,0);
+                    dtao12=dtao1+dtao2;
+                    ntao12=(tao0-dtao12)/dt;
+                    if(ntao12<(n3-1) && ntao12>0){
+                        ntaoceil=ceil(ntao12);
+                        ntaofloor=floor(ntao12);
+                        wceil=1.0/(abs(ntaoceil-ntao12)+wmin);
+                        wfloor=1.0/(abs(ntaofloor-ntao12)+wmin);
+                        wsum=wceil+wfloor;
+                        wceil=wceil/wsum;
+                        wfloor=wfloor/wsum;
+
+                        recoverdatatx(in,jn,k)=recoverdatatx(in,jn,k)\
+                            +datatp(ip,jp,ntaoceil)*wceil\
+                            +datatp(ip,jp,ntaofloor)*wfloor;
+                    }
+    }}}}}
+}
+
+void slantstack3d_stack(struct slantstack3d &par){
+    slantstack3d_stack_L_operator(par.datatp,par.datatx,\
     par.ptrace_coord,par.pline_coord,par.ntrace_coord,par.nline_coord,\
     par.dt);
 }
-
+void slantstack3d_recover(struct slantstack3d &par){
+    slantstack3d_recover_LT_operator(par.recoverdatatx,par.datatp,\
+    par.ptrace_coord,par.pline_coord,par.ntrace_coord,par.nline_coord,\
+    par.dt);
+}
 
 #endif
