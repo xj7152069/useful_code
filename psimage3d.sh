529df19b5764fc17298cab631d57356464495c85
项健 <41769115+xj7152069@users.noreply.github.com>
2022-05-21 10:26:17 +0800

    Update psimage3d.sh

--- psimage3d.sh
+++ psimage3d.sh
@@ -1,21 +1,21 @@
-filename="data3d.3dtp.cut.bin"
+filename="model.3d.nz101.nx301.ny301.bin"
 vplname="${filename}.vpl"
 epsname="${filename}.eps"
-:<<!
-echo n1=3000 d1=0.0005 o1=0\
- n2=200 o2=1000 d2=-5\
- n3=200 o3=0 d3=5\
+
+echo n1=101 o1=0 d1=5\
+ n2=301 o2=0 d2=5\
+ n3=301 o3=0 d3=5\
  in=$filename data_format=native_float>rsftp
 
-sfbyte < rsftp gainpanel=a pclip=96. |\
-sfgrey3 frame1=0 frame2=200 frame3=00 color= flat=n dframe=0\
- label1="Time (s)" label2="Offset (m)" label3="CMP (m)" labelfat=2 gridfat=2\
- point1=0.65 point2=0.7 screenratio=1\
+sfbyte < rsftp gainpanel=a pclip=100 |\
+sfgrey3 frame1=0 frame2=100 frame3=100 color=g flat=n dframe=0\
+ label1="depth (m)" label2="X (m)" label3="Y (m)" labelfat=2 gridfat=2\
+ point1=0.65 point2=0.7 screenratio=1 \
  framelabel1=n	framelabel2=n framelabel3=n \
  framelabelcol=32767 font=3\
  >$vplname 
-!
 
+:<<!
 echo n1=3000 d1=0.001 o1=0\
  n2=81 o2=-0.000825 d2=0.000015\
  n3=41 o3=-0.0003 d3=0.000015\
@@ -29,5 +29,5 @@ sfbyte < rsftp gainpanel=a pclip=99. |\
  framelabel1=n	framelabel2=n framelabel3=n \
  framelabelcol=32767 font=3\
  >$vplname 
-
+!
 vpconvert $vplname bgcolor=w color=y $epsname
