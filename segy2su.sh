b70bd18d17b1b11f29ad96ea2b1d84d69a2d35d5
项健 <41769115+xj7152069@users.noreply.github.com>
2022-07-09 13:31:10 +0800

    Update segy2su.sh

--- segy2su.sh
+++ segy2su.sh
@@ -5,15 +5,22 @@ for spath in ${paths}
 do
 folder1="./data/${spath}/*.segy"
 folder="./data/${spath}/"
-echo $folder
-softfiles=$(ls $folder)
+softfiles=$(ls $folder1)
 for sfile in ${softfiles}
 do 
-    echo "${folder}${sfile}"
-    segyread tape=${folder}${sfile} verbose=0 endian=0>"${folder}${sfile}.su"
-    susort <"${folder}${sfile}.su">"${folder}${sfile}.su.sort" tracf
-    #segyhdrs <${sfile}
-    #segyname="./segy/${sfile}.segy"
-    #segywrite <${sfile} tape=$segyname endian=0
+    echo "${sfile}"
+#    segyread tape=${folder}${sfile} verbose=0 endian=0>"${folder}${sfile}.su"
+#    susort <"${folder}${sfile}.su">"${folder}${sfile}.su.sort" tracf
+#   sustrip <"${sfile}.su.sort">"${sfile}.bin"
+    filename="${sfile}.bin"
+    epsname="${filename}.eps"
+    figure="psimage"
+    $figure< $filename \
+    n1=4096 wbox=100 hbox=50 axeswidth=9 labelfont=Times-Roman \
+    legend=0 lwidth=1 lheight=20 lstyle=vertright lx=22.0 \
+    f1=0 d1=0.002 f2=0 d2=1 x2beg=9690 x2end=10982\
+    label1='Time (s)' label2='Trace' \
+    labelsize=75 \
+    perc=98>$epsname
 done
 done
